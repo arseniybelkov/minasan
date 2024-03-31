0cabe330492a814c14cb1ef60fdd7f35f0a50895
Arseniy Belkov <arseniybelkov@yandex.ru>
2024-03-31 23:08:42 +0300

    dispatcher

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
-mod bot;
+use std::sync::Arc;
 
+use teloxide::prelude::*;
+
+use crate::commands::{endpoints, Command};
+use crate::storage::ChatStorage;
+
+mod commands;
+mod storage;
 
 #[tokio::main]
 async fn main() {
+    let bot = Bot::from_env();
+    let chat_storage = Arc::new(ChatStorage::new());
 
+    let handler = dptree::entry()
+        .branch(
+            Update::filter_message().chain(
+                teloxide::filter_command::<Command, _>()
+                    .branch(dptree::case![Command::MinasanStart].endpoint(endpoints::start))
+                    .branch(dptree::case![Command::MinasanRestart].endpoint(endpoints::restart))
+                    .branch(dptree::case![Command::MinasanPoll].endpoint(endpoints::get_poll))
+                    .branch(dptree::case![Command::MinasanKill].endpoint(endpoints::kill))
+                    .branch(dptree::case![Command::Minasan].endpoint(endpoints::tag_everyone)),
+            ),
+        )
+        .branch(Update::filter_poll_answer().endpoint(endpoints::update_users));
+
+    Dispatcher::builder(bot, handler)
+        .dependencies(dptree::deps![chat_storage])
+        .build()
+        .dispatch()
+        .await;
 }
